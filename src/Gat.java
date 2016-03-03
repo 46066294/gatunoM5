ac56e814978a12b4d643160fe7295891c7ddd3e7
46066294 <46066294p@iespoblenou.org>
2016-03-03 18:45:46 +0100

    actualitzat

--- src/Gat.java
+++ src/Gat.java
@@ -1,9 +1,9 @@
 /**
  * Created by 46066294p on 01/03/16.
  */
-public abstract class Gat implements Miolable{
-
+public abstract class Gat implements Miolable, Caminable{
 
     //metodes
     public abstract void mostra();
+
 }
--- src/GatPelutx.java
+++ src/GatPelutx.java
@@ -1,5 +1,21 @@
 /**
  * Created by 46066294p on 01/03/16.
  */
-public class GatPelutx {
+public class GatPelutx extends Gat{
+
+    @Override
+    public void mostra() {
+        System.out.println("Soc un gat de pelutx");
+    }
+
+    @Override
+    public void caminar() {
+        System.out.println("...no que camino");
+    }
+
+    @Override
+    public void miau() {
+        SenseMiau senseMiau = new SenseMiau();
+        System.out.println();
+    }
 }
